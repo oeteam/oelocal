3583e6c3548a10a08670da57641a0f80851a64c3
neethujohnson <jollyjohnsonneethu@gmail.com>
2019-04-05 15:04:26 +0530

    baseurl change

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ date_default_timezone_set('asia/dubai');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/works/otelseasy/';
+$config['base_url'] = 'http://localhost/works/oelocal/';
 
 /*
 |--------------------------------------------------------------------------
