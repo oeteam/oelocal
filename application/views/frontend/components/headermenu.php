bca02acb0fac31976ce94218805bfb16ea695bbb
neethujohnson <jollyjohnsonneethu@gmail.com>
2019-04-15 18:22:28 +0530

    notification drop down update

--- application/views/frontend/components/headermenu.php
+++ application/views/frontend/components/headermenu.php
@@ -12,6 +12,188 @@
 	.msgbox.read {
       background: #f2f2f2;
 }
+ <style>
+    .timeline {
+      list-style: none;
+      padding-left: 0;
+      position: relative;
+    }
+    .timeline:after {
+      content: "";
+      height: auto;
+      width: 1px;
+      background: #e3e3e3;
+      position: absolute;
+      top: 5px;
+      left: 30px;
+      bottom: 25px;
+    }
+    .timeline.timeline-sm:after {
+      left: 12px;
+    }
+    .timeline li {
+      position: relative;
+      padding-left: 70px;
+      margin-bottom: 20px;
+    }
+    .timeline li:after {
+      content: "";
+      width: 12px;
+      height: 12px;
+      border-radius: 50%;
+      background: #e3e3e3;
+      position: absolute;
+      left: 24px;
+      top: 5px;
+    } 
+    .timeline .timeline-icon img {
+       border-radius: 50%;
+       height: 30px;
+    }
+    .timeline li .timeline-date {
+      display: inline-block;
+      width: 100%;
+      color: #a6a6a6;
+      font-style: italic;
+      font-size: 13px;
+    }
+    .timeline .timeline-icons li {
+      padding-top: 7px;
+    }
+    .timeline.timeline-icons li:after {
+      width: 32px;
+      height: 32px;
+      background: #fff;
+      border: 1px solid #e3e3e3;
+      left: 14px;
+      top: 0;
+      z-index: 11;
+    }
+    .timeline.timeline-icons li .timeline-icon {
+      position: absolute;
+      left: 23.5px;
+      top: 1px;
+      z-index: 12;
+    }
+    .timeline.timeline-icons li .timeline-icon [class*=glyphicon] {
+      top: -1px !important;
+    }
+    .timeline.timeline-icons.timeline-sm li {
+      padding-left: 40px;
+      margin-bottom: 10px;
+    }
+    .timeline.timeline-icons.timeline-sm li:after {
+      left: -5px;
+    }
+    .timeline.timeline-icons.timeline-sm li .timeline-icon {
+      left: -4.5px;
+    }
+    .timeline.timeline-advanced li {
+      padding-top: 0;
+    }
+    .timeline.timeline-advanced li:after {
+      background: #fff;
+      border: 1px solid #29b6d8;
+    }
+    .timeline.timeline-advanced li:before {
+      content: "";
+      width: 52px;
+      height: 52px;
+      border: 10px solid #fff;
+      position: absolute;
+      left: 4px;
+      top: -10px;
+      border-radius: 50%;
+      z-index: 12;
+    }
+    .timeline.timeline-advanced li .timeline-icon {
+      color: #29b6d8;
+    }
+    .timeline.timeline-advanced li .timeline-date {
+      width: 75px;
+      position: absolute;
+      right: 5px;
+      top: 3px;
+      text-align: right;
+    }
+    .timeline.timeline-advanced li .timeline-title {
+      font-size: 17px;
+      margin-bottom: 0;
+      padding-top: 5px;
+      font-weight: bold;
+    }
+    .timeline.timeline-advanced li .timeline-subtitle {
+      display: inline-block;
+      width: 100%;
+      color: #a6a6a6;
+    }
+    .timeline.timeline-advanced li .timeline-content {
+      margin-top: 10px;
+      margin-bottom: 10px;
+      padding-right: 70px;
+    }
+    .timeline.timeline-advanced li .timeline-content p {
+      margin-bottom: 3px;
+    }
+    .timeline.timeline-advanced li .timeline-content .divider-dashed {
+      padding-top: 0px;
+      margin-bottom: 7px;
+      width: 200px;
+    }
+    .timeline.timeline-advanced li .timeline-user {
+      display: inline-block;
+      width: 100%;
+      margin-bottom: 10px;
+    }
+    .timeline.timeline-advanced li .timeline-user:before,
+    .timeline.timeline-advanced li .timeline-user:after {
+      content: " ";
+      display: table;
+    }
+    .timeline.timeline-advanced li .timeline-user:after {
+      clear: both;
+    }
+    .timeline.timeline-advanced li .timeline-user .timeline-avatar {
+      border-radius: 50%;
+      width: 32px;
+      height: 32px;
+      float: left;
+      margin-right: 10px;
+    }
+    .timeline.timeline-advanced li .timeline-user .timeline-user-name {
+      font-weight: bold;
+      margin-bottom: 0;
+    }
+    .timeline.timeline-advanced li .timeline-user .timeline-user-subtitle {
+      color: #a6a6a6;
+      margin-top: -4px;
+      margin-bottom: 0;
+    }
+    .timeline.timeline-advanced li .timeline-link {
+      margin-left: 5px;
+      display: inline-block;
+    }
+    .timeline-load-more-btn {
+      margin-left: 70px;
+    }
+    .timeline-load-more-btn i {
+      margin-right: 5px;
+    }
+    /* -----------------------------------------
+       Dropdown
+    ----------------------------------------- */
+    .dropdown-menu{
+        padding:0 0 0 0;
+    }
+    a.dropdown-menu-header {
+        background: #f7f9fe;
+        font-weight: bold;
+        border-bottom: 1px solid #e3e3e3;
+    }
+    .dropdown-menu > li a {
+        padding: 10px 20px;
+    }
+  </style>
 </style>
 <head>
     <script src="<?php echo base_url(); ?>skin/js/common.js"></script>
@@ -73,32 +255,42 @@
               </ul>
             </li>
             <li class="dropdown">
-              <a data-toggle="dropdown" class="dropdown-toggle" href="#"><i class="fa fa-bell-o" aria-hidden="true"></i><span class="d-mes active"><?php echo $all_count ?></span></a>
-              <ul class="dropdown-menu">
-                  <?php  if ($notify_count!=0) {
-                      foreach ($notify as $key => $value) {
-                        if ($value->booking_flag==1) { ?>
-                          <?php if ($value->readed==2){ ?>
-                                <li class='msgbox  offset-0'>
-                              <?php }else{ ?><li class='msgbox read offset-0'><?php } ?>
-                              <a href='<?php echo base_url(); ?>Payment/agent_booking_view?id=<?php echo $value->bk_id?>'><img src='<?php echo base_url(); ?>uploads/rooms/<?php echo $value->room_id ?>/<?php echo $value->images ?>' alt='' width='30' class='left margright10 roundav'/>
-                            <span class='opensans size13 dark'> <?php echo $value->hotel_name ?></span><br/><span class='opensans size12'><?php echo $value->hotel_name ?> Approved your booking</span></a></li>
-                            <?php } if ($value->booking_flag==0) { ?>
-                               <?php if ($value->readed==2){ ?>
-                                <li class='msgbox  offset-0'>
-                              <?php }else{ ?><li class='msgbox read offset-0'><?php } ?><a href='<?php echo base_url(); ?>Payment/agent_booking_view?id=<?php echo $value->bk_id?>'><img src='<?php echo base_url(); ?>uploads/rooms/<?php echo $value->room_id ?>/<?php echo $value->images ?>' alt='' width='30' class='left margright10 roundav'/>
-                            <span class='opensans size13 dark'> Rejected request </span><br/><span class='opensans size12'><?php echo $value->hotel_name ?> rejected your booking</span></a></li>
-
-                            <?php } if ($value->booking_flag==2) { ?>
-                             <?php if ($value->readed==2){ ?>
-                                <li class='msgbox  offset-0'>
-                              <?php }else{ ?><li class='msgbox read offset-0'><?php } ?><a href='<?php echo base_url(); ?>Payment/agent_booking_view?id=<?php echo $value->bk_id?>'><img src='<?php echo base_url(); ?>uploads/rooms/<?php echo $value->room_id ?>/<?php echo $value->images ?>' alt='' width='30' class='left margright10 roundav'/>
-                            <span class='opensans size13 dark'>New booking request</span><br/><span class='opensans size12'>You are booked <?php echo $value->hotel_name ?></span></a></li>
-              
-                            <?php } }  ?><li class='text-center view-more'><a href='<?php echo base_url(); ?>Payment/all_notification'>view more</a></li>
-                            <?php }else { echo "No Notifications"; } ?>
-              </ul>
-            </li>
+                            <a data-toggle="dropdown" class="dropdown-toggle" href="#"><i class="fa fa-bell-o" aria-hidden="true"></i><span class="d-mes active"><?php echo $all_count ?></span></a>
+                            <ul class="dropdown-menu">
+                              <li role="presentation">
+                                <a href="#" class="dropdown-menu-header">Notifications</a>
+                              </li>
+                                <?php  if ($notify_count!=0) { ?>
+                                  <ul class="timeline timeline-icons timeline-sm" style="margin:10px;width:210px">
+                                    <?php foreach ($notify as $key => $value) {
+                                      if ($value->booking_flag==1) { ?>
+                                        <?php if ($value->readed==2){ ?>
+                                              <li class='msgbox  offset-0'>
+                                            <?php }else{ ?><li class='msgbox read offset-0'><?php } ?>
+                                            <a href='<?php echo base_url(); ?>Payment/agent_booking_view?id=<?php echo $value->bk_id?>'><span class="timeline-icon"><img src='<?php echo base_url(); ?>uploads/rooms/<?php echo $value->room_id ?>/<?php echo $value->images ?>' alt='' width='30' class='left margright10 roundav'/></span>
+                                          <span class='opensans size13 dark'><b><?php echo $value->hotel_name ?></b></span><br/><span class='opensans size12'><?php echo $value->hotel_name ?> Approved your booking</span></a></li>
+                                      <?php } 
+                                      if ($value->booking_flag==0) { ?>
+                                        <?php if ($value->readed==2){ ?>
+                                              <li class='msgbox  offset-0'>
+                                            <?php }else{ ?><li class='msgbox read offset-0'><?php } ?><a href='<?php echo base_url(); ?>Payment/agent_booking_view?id=<?php echo $value->bk_id?>'><span class="timeline-icon"><img src='<?php echo base_url(); ?>uploads/rooms/<?php echo $value->room_id ?>/<?php echo $value->images ?>' alt='' width='30' class='left margright10 roundav'/></span>
+                                          <span class='opensans size13 dark'><b> Rejected request</b> </span><br/><span class='opensans size12'><?php echo $value->hotel_name ?> rejected your booking</span></a></li>
+                                      <?php }
+                                      if ($value->booking_flag==2) { ?>
+                                        <?php if ($value->readed==2){ ?>
+                                              <li class='msgbox  offset-0'>
+                                              <?php }else{ ?><li class='msgbox read offset-0'><?php } ?><a href='<?php echo base_url(); ?>Payment/agent_booking_view?id=<?php echo $value->bk_id?>'><span class="timeline-icon"><img src='<?php echo base_url(); ?>uploads/rooms/<?php echo $value->room_id ?>/<?php echo $value->images ?>' alt='' width='30' class='left margright10 roundav'/></span>
+                                            <span class='opensans size13 dark'><b>New booking request</b></span></br><span class='opensans size12 dark'>You are booked <?php echo $value->hotel_name ?></span></a></li>
+                                      <?php } ?>
+                                    <?php }  ?>
+                                  </ul>
+                                     <li class='text-center viewmore'><a href='<?php echo base_url(); ?>Payment/all_notification'>view more</a></li>
+                                <?php }  else { ?>
+                                   <li class='text-center viewmore'><a href="#">No Notifications</a></li> 
+                               <?php } ?>
+                             
+                            </ul>
+           </li>
            <li class="dropdown">
                     <?php if ($flag_img!=""){ ?>
                     <a data-toggle="dropdown" class="dropdown-toggle"><img src="<?php echo base_url();?>assets/images/flg/<?php echo $flag_img; ?>.png"><span  style="color: #607D8B;"> / <?php echo $flag ?></span><b class="lightcaret mt-2 xml-default"></b></a>
