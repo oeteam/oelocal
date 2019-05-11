3a6a01665b9d8dfdebfda0453636580b51544dba
neethujohnson <jollyjohnsonneethu@gmail.com>
2019-05-11 16:17:03 +0530

    events page design update

--- application/views/events.php
+++ application/views/events.php
@@ -3,21 +3,25 @@
 
 <style>
   .banner-bottom {
-    background-image: url("<?php echo base_url() ?>agentLoginStyle/img/baner3.jpg");
+    background-image: url("<?php echo base_url() ?>agentLoginStyle/img/eventheader.jpg");
     height: 60%;
     position: relative;
   }
   .aboutus-title {
         color: #fff;
         position: absolute;
-        top: 50%;
+        top: 35%;
         left: 50%;
         transform: translate(-50%, -50%);
-        font-size: 5em;
+        font-size: 4em;
   }
   .aboutus-desc {
-    height: 50%;
-    margin: 7em auto 8em;
+    position: absolute;
+    top: 55%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 2em;
+    color: white;
   }
   .aboutus-desc p::first-letter {
     /*font-size: 200%;*/
@@ -26,16 +30,18 @@
     font-weight: bold;
     font-size: 15px;
     margin-left: 20px;
+    color: #fff;
   }
 
 </style>
 
       <!--baner-->
                   <!--carosel fade-->
-             <!-- <img src="<?php echo base_url() ?>uploads/about/<?php echo $view[0]->wall_image ?>" > -->
+             <!-- <img src="<?php //echo base_url() ?>uploads/about/<?php //echo $view[0]->wall_image ?>" > -->
       <!--baner-->
       <section class="banner-bottom">
         <p class="aboutus-title">Events</p>
+        <div class="aboutus-desc">Dont miss the oppurtunity to be part of this events.</div>
       </section>
       <br><br>
       <section>
@@ -44,16 +50,14 @@
                         <?php if(!empty($view)){
                           foreach($view as $event_value){ ?>
                           <a href="<?php echo base_url()?>events_view/<?php echo $event_value->id ?>"> <div class="col-lg-4">
-                              <div class="cuadro_intro_hover" style="height:230px">
+                              <div class="cuadro_intro_hover" style="height:245px">
                                  <img src="<?php echo base_url(); ?>uploads/events/<?php echo $event_value->id."/".$event_value->event_image ?>" class="img-responsive" width="100%"  alt=""/>
                                  <div class="caption">
                                     <div class="blur"></div>
-                                    <div class="caption-text">
+                                    <div class="caption-text text-left">
                                        <h3 class="news-title"><?php echo $event_value->event_name; ?></h3>
-                                       <p class="news-dec">
-                                         Address: <?php echo $event_value->event_adrs; ?><br>
-                                         <?php echo date("d M Y", strtotime($event_value->start_date)); ?> to <?php  echo date("d M Y", strtotime($event_value->end_date)); ?>
-                                       </p>
+                                        <p><i class="fa fa-map-marker"></i>&nbsp;&nbsp;<span><?php echo $event_value->event_adrs; ?></span></p>
+                                        <p><i class="fa fa-calendar" aria-hidden="true"></i>&nbsp;&nbsp;<span><?php echo date("d M Y", strtotime($event_value->start_date)); ?> to <?php  echo date("d M Y", strtotime($event_value->end_date)); ?></span></p>
                                     </div>
                                  </div>
                               </div></a>
