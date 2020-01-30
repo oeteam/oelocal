1a1fe3f61e8245c2b561deb44feeb7e1515ac81b
OtelseasySystem <otelseasysystem@gmail.com>
2020-01-30 12:12:30 +0530

    env

--- index.php
+++ index.php
@@ -58,7 +58,7 @@ set_time_limit(900000);
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	define('ENVIRONMENT', 'development');
 
 /*
  *---------------------------------------------------------------
